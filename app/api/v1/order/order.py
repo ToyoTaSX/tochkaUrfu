88634b08bbd1fe048e263d04861356a2a17d9694
ToyoTaSX <toyotas_2004@mail.ru>
2025-05-22 13:48:35 +0500

    Update order.py

--- app/api/v1/order/order.py
+++ app/api/v1/order/order.py
@@ -70,7 +70,6 @@ async def order(order: CreateOrderScheme, user: User = Depends(get_current_user)
         order_ = await sell_order(order, user)
     if order_.status == OrderStatusEnum.CANCELLED:
         raise HTTPException(422, detail='ORDER CANCELLED')
-    time.sleep(1)
     return {
         "success": True,
         "order_id": str(order_.id)
